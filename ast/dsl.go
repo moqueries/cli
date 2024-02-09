a35ed1117336134e4b5aeda150abcb714c2af7b4
Dwayne Schultz <myshkin5@users.noreply.github.com>
2024-02-09 10:39:32 -0700

    Appease linters (@#$%!)

--- ast/dsl.go
+++ ast/dsl.go
@@ -24,8 +24,6 @@ func (d AssignDSL) Tok(tok token.Token) AssignDSL {
 }
 
 // Rhs specifies the right-hand expressions in the assignment
-//
-//nolint:revive // Rhs is the capitalization from dst (which this wraps)
 func (d AssignDSL) Rhs(rhs ...dst.Expr) AssignDSL {
 	d.Obj.Rhs = append(d.Obj.Rhs, rhs...)
 	return d
